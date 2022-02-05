020fbabe2e28725dfa89a83405e946b9d67cd5b6
John-Kimani <kimwaweru100@gmail.com>
2022-02-06 00:37:45 +0300

    styles application icon, resets the default  app.html code and tests app folder files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Quote-App';
+  quote = 'In the begining, there was terminal'
 }
