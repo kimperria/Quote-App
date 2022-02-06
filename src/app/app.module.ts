c90936797e6cf4854385ca3be681b495e4d47641
John-Kimani <kimwaweru100@gmail.com>
2022-02-06 04:32:16 +0300

    generates user form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteLikesComponent } from './quote-likes/quote-likes.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
-    QuoteLikesComponent
+    QuoteLikesComponent,
+    QuoteFormComponent
   ],
   imports: [
     BrowserModule,
